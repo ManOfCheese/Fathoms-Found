cea25f71e6853b88a9f17f5b6df280a32a169d18
Mart <vanderhoekmart@gmail.com>
2022-02-17 17:07:03 +0100

    Revert "Merge branch 'Mart' into Playtest"
    This reverts commit 9011d4433c6c6b66252b86f2b1b5bcfc12b8a9c1, reversing
    changes made to 53dc44d9c349d81630048aedbbd4689358bb6f9e.

--- Quantum Mirror/Assets/Scripts/ItemInteractor.cs
+++ Quantum Mirror/Assets/Scripts/ItemInteractor.cs
@@ -10,7 +10,6 @@ public class ItemInteractor : MonoBehaviour
     public InteractObject interactObject;
     public LayerMask interactMask;
 
-
     // Start is called before the first frame update
     void Start()
     {
@@ -18,20 +17,21 @@ public class ItemInteractor : MonoBehaviour
     }
 
     // Update is called once per frame
-    void FixedUpdate()
+    void Update()
     {
+        Debug.DrawRay(cam.transform.position, cam.transform.forward * interactRange, Color.red, 0);
 
-        RaycastHit hit;    
 
-        if (!interactPrompt.activeSelf && Physics.Raycast(cam.transform.position, cam.transform.forward, out hit, interactRange, interactMask))
+        RaycastHit hit;
+        if (!interactPrompt.activeSelf &&
+               Physics.Raycast(cam.transform.position, cam.transform.forward, out hit, interactRange, interactMask))
         {
             interactPrompt.gameObject.SetActive(true);
-            Debug.DrawRay( cam.transform.position, cam.transform.forward * interactRange, Color.yellow );
             interactObject = hit.transform.GetComponent<InteractObject>();
         }
-        else if (interactPrompt.activeSelf && !Physics.Raycast(cam.transform.position, cam.transform.forward, out hit, interactRange, interactMask))
+        else if (interactPrompt.activeSelf &&
+            !Physics.Raycast(cam.transform.position, cam.transform.forward, out hit, interactRange, interactMask))
         {
-            Debug.DrawRay( cam.transform.position, cam.transform.forward * interactRange, Color.white );
             interactPrompt.gameObject.SetActive(false);
             interactObject = null;
         }
@@ -40,8 +40,7 @@ public class ItemInteractor : MonoBehaviour
         {
             if (Input.GetKeyDown(KeyCode.E))
             {
-                Debug.DrawRay( cam.transform.position, cam.transform.forward * interactRange, Color.white );
-                interactObject.ActivateObject();
+                interactPrompt.SetActive(false);
             }
         }
     }
