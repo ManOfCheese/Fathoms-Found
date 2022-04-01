11b16453a9fdd1ee4068e3497d0ee2c73f473f36
LAPTOP-RH2V763A\samwa <samwalet16@gmail.com>
2022-04-01 13:27:01 +0200

    - Updated add source modifer to work witwh multiple sources per object.

--- Quantum Mirror/Assets/Scripts/Objects/ObjectModifiers/AddSourceModifier.cs
+++ Quantum Mirror/Assets/Scripts/Objects/ObjectModifiers/AddSourceModifier.cs
@@ -5,15 +5,19 @@ using UnityEngine;
 public class AddSourceModifier : ObjModifier
 {
 
-	public GameObject logicObject;
+	public GameObject sourcePrefab;
+	public GameObject parentUnderObject;
     public Property becomeSourceOf;
+	public float radius;
     public float valueAtCentre;
     public AnimationCurve fallOff;
 
 	public override void OnThresholdCross()
 	{
 		base.OnThresholdCross();
-		Source source = logicObject.AddComponent<Source>();
+		GameObject newSource = Instantiate( sourcePrefab, parentUnderObject.transform.position, parentUnderObject.transform.rotation, parentUnderObject.transform.parent );
+		newSource.GetComponent<SphereCollider>().radius = radius;
+		Source source = newSource.GetComponent<Source>();
 		source.sourceOf = becomeSourceOf;
 		source.valueAtCentre = valueAtCentre;
 		source.fallOff = fallOff;
