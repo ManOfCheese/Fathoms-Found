32947e83d7d1179d3a41423f33cf6eaafcf92cb4
LAPTOP-RH2V763A\samwa <samwalet16@gmail.com>
2022-04-22 16:27:47 +0200

    Implemented graph's that display sensor data.

--- Quantum Mirror/Assets/Scripts/Objects/Tools/GenericSensor.cs
+++ Quantum Mirror/Assets/Scripts/Objects/Tools/GenericSensor.cs
@@ -6,7 +6,7 @@ using UnityEngine.UI;
 public class GenericSensor : MonoBehaviour
 {
     [Header( "References" )]
-    public GameObject camera;
+    public GameObject cam;
     public PropertyInfo[] propertiestoDetect;
     public Text sensorText;
     public float detectionRange;
@@ -17,7 +17,7 @@ public class GenericSensor : MonoBehaviour
     void Update()
     {
         RaycastHit hit;
-        if ( Physics.Raycast( camera.transform.position, camera.transform.forward, out hit, detectionRange ) ) {
+        if ( Physics.Raycast( cam.transform.position, cam.transform.forward, out hit, detectionRange ) ) {
             if ( hit.transform.GetComponentInChildren<Source>() )
                 sensorText.text = prefix + hit.transform.GetComponentInChildren<Source>().sourceOf.propertyName + suffix;
             else if ( hit.transform.GetComponentInChildren<Object>() )
