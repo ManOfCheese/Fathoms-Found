6db50f311493d4696e720fc224d8ca22de93fe28
LAPTOP-RH2V763A\samwa <samwalet16@gmail.com>
2022-04-26 16:41:50 +0200

    Working on sampling

--- Quantum Mirror/Assets/Scripts/SampleTool.cs
+++ Quantum Mirror/Assets/Scripts/SampleTool.cs
@@ -1,22 +1,56 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class SampleTool : MonoBehaviour
 {
 
+	[Header( "Sampler  Settings" )]
+	public GameObject sampleTool;
+	public Transform ballTransform;
+	public GameObject sampleBallPrefab;
+	public float sampleDuration;
+	public float scaleModifier;
+
+	[Header( "Sensor Settings" )]
 	public Transform raycastFrom;
 	public GameObject sensor;
 	public LayerMask layerMask;
 	public float sensorRange;
 
+	private GameObject objectInRay;
+	private GameObject sampleBall;
+
 	private void Update()
 	{
 		RaycastHit hit;
 		if ( Physics.Raycast( raycastFrom.position, raycastFrom.forward, out hit, sensorRange, layerMask ) )
+		{
 			sensor.transform.position = hit.point;
+			objectInRay = hit.transform.gameObject;
+		}
 		else
+		{
 			sensor.transform.position = raycastFrom.transform.position + ( raycastFrom.forward * sensorRange );
+			objectInRay = null;
+		}
+	}
+
+	public void Sample( InputAction.CallbackContext value )
+	{
+		if ( value.performed && objectInRay != null )
+		{
+			if ( sampleBall != null )
+				Destroy( sampleBall );
+			sampleBall = Instantiate( sampleBallPrefab, ballTransform.position, ballTransform.rotation, sampleTool.transform );
+			Transform sample = Instantiate( objectInRay, sampleBall.transform.position, sampleBall.transform.rotation, sampleBall.transform ).transform;
+			MeshRenderer sampleCollider = sample.GetComponent<MeshRenderer>();
+			MeshRenderer ballCollider = sampleBall.GetComponent<MeshRenderer>();
+			sample.transform.localScale *=
+				Mathf.Max( ballCollider.bounds.size.x, ballCollider.bounds.size.y, ballCollider.bounds.size.z ) /
+				Mathf.Max( sampleCollider.bounds.size.x, sampleCollider.bounds.size.y, sampleCollider.bounds.size.z ) * scaleModifier;
+		}
 	}
 
 	private void OnDrawGizmos()
