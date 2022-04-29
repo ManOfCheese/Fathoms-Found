c40ce26a3f5f5327976f757d19efccd56e5b192b
LAPTOP-RH2V763A\samwa <samwalet16@gmail.com>
2022-04-29 11:48:27 +0200

    big merge  - Hierarchy restructuring  - Prefab renaming  - Quick error fixes

--- Quantum Mirror/Assets/Scripts/SampleTool.cs
+++ Quantum Mirror/Assets/Scripts/SampleTool.cs
@@ -44,13 +44,17 @@ public class SampleTool : MonoBehaviour
 		{
 			if ( sampleBall != null )
 				Destroy( sampleBall );
-			sampleBall = Instantiate( sampleBallPrefab, ballTransform.position, ballTransform.rotation, sampleTool.transform );
+			sampleBall = Instantiate( sampleBallPrefab, ballTransform.position, ballTransform.rotation, ballTransform.transform );
 			Transform sample = Instantiate( objectInRay, sampleBall.transform.position, sampleBall.transform.rotation, sampleBall.transform ).transform;
-			MeshRenderer sampleCollider = sample.GetComponent<MeshRenderer>();
-			MeshRenderer ballCollider = sampleBall.GetComponent<MeshRenderer>();
-			sample.transform.localScale *=
-				Mathf.Max( ballCollider.bounds.size.x, ballCollider.bounds.size.y, ballCollider.bounds.size.z ) /
-				Mathf.Max( sampleCollider.bounds.size.x, sampleCollider.bounds.size.y, sampleCollider.bounds.size.z ) * scaleModifier;
+			
+			if ( sample.GetComponent<MeshRenderer>() && sampleBall.GetComponent<MeshRenderer>() )
+			{
+				MeshRenderer sampleCollider = sample.GetComponent<MeshRenderer>();
+				MeshRenderer ballCollider = sampleBall.GetComponent<MeshRenderer>();
+				sample.transform.localScale *=
+					Mathf.Max( ballCollider.bounds.size.x, ballCollider.bounds.size.y, ballCollider.bounds.size.z ) /
+					Mathf.Max( sampleCollider.bounds.size.x, sampleCollider.bounds.size.y, sampleCollider.bounds.size.z ) * scaleModifier;
+			}
 		}
 	}
 
